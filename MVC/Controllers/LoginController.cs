c28dd74ae201c6703fcdbdaac942d46f437786d9
Mason7579 <127904628+Mason7579@users.noreply.github.com>
2023-09-18 20:43:26 -0500

    Add files via upload

---
+++ MVC/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cosc_4353_project.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+        public IActionResult register()
+        {
+            return View();
+        }
+    }
+}
