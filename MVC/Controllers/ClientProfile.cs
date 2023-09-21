4f607dfbc069d246b256ee7409e89e69d4dd6adc
Garvey21 <120845043+Garvey21@users.noreply.github.com>
2023-09-21 10:52:52 -0500

    Add files via upload

---
+++ MVC/Controllers/ClientProfile.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cosc_4353_project.Controllers
+{
+    public class ClientProfileController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+    }
+}
