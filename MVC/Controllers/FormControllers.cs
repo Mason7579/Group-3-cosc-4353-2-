3381581e4ceea43f1c63badb431851b334a42b57
Germa6666 <129021400+Germa6666@users.noreply.github.com>
2023-09-21 19:40:46 -0500

    FuelQuoteController
    FuelQuoteController

---
+++ MVC/Controllers/FormControllers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace cosc_4353_project.Controllers
+{
+    public class FuelQuoteFormControllers : Controller
+    {
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
