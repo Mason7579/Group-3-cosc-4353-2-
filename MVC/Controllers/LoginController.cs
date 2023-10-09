dea2a38a9d04b15454afe69b14f45a644aa30ffd
NGk71 <89110787+NGk71@users.noreply.github.com>
2023-10-08 23:08:16 -0500

    Changes on login/regis/controller

--- MVC/Controllers/LoginController.cs
+++ MVC/Controllers/LoginController.cs
@@ -4,12 +4,35 @@ namespace cosc_4353_project.Controllers
 {
     public class LoginController : Controller
     {
+        [HttpGet]
         public IActionResult login()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult login(string username, string password)
+        {
+            // TODO: Add validation logic here
+
+            // For now, just returning to the login view with a success message
+            ViewBag.Message = "Login successful!";
+            return View();
+        }
+
+        [HttpGet]
         public IActionResult register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult register(string username, string password, string confirmPassword)
+        {
+            // TODO: Add validation logic here
+
+            // For now, just returning to the register view with a success message
+            ViewBag.Message = "Registration successful!";
             return View();
         }
     }
