6419d1ae053098fc9e277d17605b47fd9c6ae14d
Mason <yzhan308@cougarnet.uh.edu>
2023-10-12 00:01:33 -0500

    unit test

---
+++ UnitTest/FuelQuoteTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class FuelQuoteTest
+    {
+        [Fact]
+        public void Test_FuelQuoteHistory_return_history()
+        {
+            var controller = new FuelQuoteController();
+            var result = controller.history() as ViewResult;
+            var historyList = result.Model as FuelQuoteHistory;
+            var fuelQuote = historyList.History[0];
+            Assert.Equal(139, fuelQuote.Gallons_Requested);
+            Assert.Equal("12/08/2022", fuelQuote.Delivery_Date);
+            Assert.Equal(2.90, fuelQuote.Suggested_Price);
+            Assert.Equal(403.1, fuelQuote.Total_Amount_Due);
+            Assert.Equal("123 HelloWorld St. TX", fuelQuote.Delivery_Address);
+        }
+    }
+}
---
+++ UnitTest/GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using Xunit;
+global using cosc_4353_project.Controllers;
+global using cosc_4353_project.Models;
