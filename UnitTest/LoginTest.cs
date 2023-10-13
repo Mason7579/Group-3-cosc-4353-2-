255d401083b4ab7d265646c1e8a89210d14d14de
Germa6666 <129021400+Germa6666@users.noreply.github.com>
2023-10-13 09:17:28 -0500

    LoginTest
    LoginTest

---
+++ UnitTest/LoginTest.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using cosc_4353_project.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTest
+{
+    public class LoginTest
+    {
+        [Fact]
+        public void Test_Login_return_view()
+        {
+            var controller = new LoginController();
+            var result = controller.login() as ViewResult;
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void Test_Login_with_credentials_return_view()
+        {
+            var username = "testusername";
+            var password = "testpassword";
+            var controller = new LoginController();
+            var result = controller.login(username, password) as ViewResult;
+
+            Assert.NotNull(result);
+            Assert.Equal("Login successful!", result.ViewData["Message"]);
+        }
+
+    }
+}
