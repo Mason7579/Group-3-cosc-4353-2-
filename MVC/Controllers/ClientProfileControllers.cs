3a1efe8c2cd21f144278df69604b9a0e2e5fd128
Garvey21 <120845043+Garvey21@users.noreply.github.com>
2023-10-26 09:54:48 -0500

    Update ClientProfileControllers.cs

--- MVC/Controllers/ClientProfileControllers.cs
+++ MVC/Controllers/ClientProfileControllers.cs
@@ -1,24 +1,153 @@
 using cosc_4353_project.Models;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using NpgsqlTypes;
+using System.Data;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
 
 namespace cosc_4353_project.Controllers
 {
     public class ClientProfileController : Controller
     {
+        private static string _connectionString = @"Server=cosc4353-group-3.postgres.database.azure.com;Port=5432;Database=cosc4353-homework;User Id=vscode@cosc4353-group-3;Password=vscode123;";
+
+        [HttpGet]
         public IActionResult Profile()
+        {
+            string username = GetLoggedInUsername();
+            ClientProfileModel profile = GetClientProfile(username);
+
+            if (profile == null)
+            {
+                profile = new ClientProfileModel();
+            }
+
+            return View(profile);
+        }
+
+        [HttpPost]
+        public IActionResult SaveProfile(ClientProfileModel model)
+        {
+            string username = GetLoggedInUsername();
+
+            if (ModelState.IsValid)
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(_connectionString))
+                {
+                    conn.Open();
+
+                    if (ProfileExists(username))
+                    {
+                        using (NpgsqlCommand command = new NpgsqlCommand("UPDATE profile SET full_name = @FullName, address_1 = @Address1, address_2 = @Address2, city = @City, state = @State, zipcode = @Zipcode WHERE username_pf = @Username", conn))
+                        {
+                            command.Parameters.AddWithValue("@FullName", model.FullName);
+                            command.Parameters.AddWithValue("@Address1", model.Address1);
+
+                            if (model.Address2 != null)
+                            {
+                                command.Parameters.AddWithValue("@Address2", NpgsqlDbType.Varchar, model.Address2);
+                            }
+                            else
+                            {
+                                command.Parameters.AddWithValue("@Address2", DBNull.Value);
+                            }
+
+                            command.Parameters.AddWithValue("@City", model.City);
+                            command.Parameters.AddWithValue("@State", model.State);
+                            command.Parameters.AddWithValue("@Zipcode", model.Zipcode);
+                            command.Parameters.AddWithValue("@Username", username);
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                    else
+                    {
+                        using (NpgsqlCommand command = new NpgsqlCommand("INSERT INTO profile (full_name, address_1, address_2, city, state, zipcode, username_pf) VALUES (@FullName, @Address1, @Address2, @City, @State, @Zipcode, @Username)", conn))
+                        {
+                            command.Parameters.AddWithValue("@FullName", model.FullName);
+                            command.Parameters.AddWithValue("@Address1", model.Address1);
+
+                            if (model.Address2 != null)
+                            {
+                                command.Parameters.AddWithValue("@Address2", NpgsqlDbType.Varchar, model.Address2);
+                            }
+                            else
+                            {
+                                command.Parameters.AddWithValue("@Address2", DBNull.Value);
+                            }
+
+                            command.Parameters.AddWithValue("@City", model.City);
+                            command.Parameters.AddWithValue("@State", model.State);
+                            command.Parameters.AddWithValue("@Zipcode", model.Zipcode);
+                            command.Parameters.AddWithValue("@Username", username);
+
+                            command.ExecuteNonQuery();
+                        }
+                    }
+
+                    return RedirectToAction("ProfileSaved");
+                }
+            }
+
+            return View("Profile", model);
+        }
+
+        private bool ProfileExists(string username)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connectionString))
+            {
+                conn.Open();
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT COUNT(*) FROM profile WHERE username_pf = @Username", conn))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+                    return count > 0;
+                }
+            }
+        }
+
+        public IActionResult ProfileSaved()
         {
             return View();
         }
 
-        [HttpPost]
-        public IActionResult Profile(ClientProfileModel model)
+        private string GetLoggedInUsername()
         {
-            if (ModelState.IsValid)
-            {
-                return Content("Profile saved successfully!");
-            }
-            return View("Profile", model);
+            return "admin";
         }
 
+        private ClientProfileModel GetClientProfile(string username)
+        {
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connectionString))
+            {
+                conn.Open();
+                using (NpgsqlCommand command = new NpgsqlCommand("SELECT * FROM profile WHERE username_pf = @Username", conn))
+                {
+                    command.Parameters.AddWithValue("@Username", username);
+
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            ClientProfileModel profile = new ClientProfileModel
+                            {
+                                FullName = reader["full_name"].ToString(),
+                                Address1 = reader["address_1"].ToString(),
+                                Address2 = reader["address_2"].ToString(),
+                                City = reader["city"].ToString(),
+                                State = reader["state"].ToString(),
+                                Zipcode = reader["zipcode"].ToString(),
+                                Username = reader["username_pf"].ToString()
+                            };
+
+                            return profile;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 }
