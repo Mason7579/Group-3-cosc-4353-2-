af615f0ea9bc8eda20b401b2b481e5379c4e81cf
Germa6666 <129021400+Germa6666@users.noreply.github.com>
2023-10-27 09:41:33 -0500

    change

--- UnitTest/LoginTest.cs
+++ UnitTest/LoginTest.cs
@@ -1,30 +1,62 @@
-﻿using Xunit;
-using cosc_4353_project.Controllers;
+﻿using cosc_4353_project.Controllers;
+using cosc_4353_project.Models;
 using Microsoft.AspNetCore.Mvc;
+using Xunit;
 
 namespace UnitTest
 {
-    public class LoginTest
+    public class LoginControllerTest
     {
         [Fact]
-        public void Test_Login_return_view()
+        public void Test_Login_Get_ReturnsView()
         {
+            // Arrange
             var controller = new LoginController();
+
+            // Act
             var result = controller.login() as ViewResult;
+
+            // Assert
             Assert.NotNull(result);
         }
 
         [Fact]
-        public void Test_Login_with_credentials_return_view()
+        public void Test_Login_Post_WithValidCredentials_RedirectsToHistory()
         {
-            var username = "testusername";
-            var password = "testpassword";
+            // Arrange
             var controller = new LoginController();
-            var result = controller.login(username, password) as ViewResult;
+            var model = new LoginViewModel
+            {
+                Username = "admin",
+                Password = "admin123"
+            };
 
+            // Act
+            var result = controller.login(model) as RedirectToActionResult;
+
+            // Assert
             Assert.NotNull(result);
-            Assert.Equal("Login successful!", result.ViewData["Message"]);
+            Assert.Equal("history", result.ActionName);
+            Assert.Equal("Fuelquote", result.ControllerName);
         }
 
+        [Fact]
+        public void Test_Login_Post_WithInvalidCredentials_ReturnsViewWithMessage()
+        {
+            // Arrange
+            var controller = new LoginController();
+            var model = new LoginViewModel
+            {
+                Username = "invalidusername",
+                Password = "invalidpassword"
+            };
+
+            // Act
+            var result = controller.login(model) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("Invalid credentials!", controller.ViewBag.Message);
+        }
     }
 }
