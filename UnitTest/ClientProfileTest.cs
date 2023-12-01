142f90af2aa6db53ead2d6c63c1c1c203d40f884
Garvey21 <120845043+Garvey21@users.noreply.github.com>
2023-12-01 17:22:33 -0600

    Update ClientProfileTest.cs

--- UnitTest/ClientProfileTest.cs
+++ UnitTest/ClientProfileTest.cs
@@ -1,47 +1,118 @@
-using Xunit;
+using System;
+using System.Data;
 using cosc_4353_project.Controllers;
 using cosc_4353_project.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+
+public interface IYourRepository
+{
+
+    ClientProfileModel GetProfileByUsername(string username);
+
+}
 
 namespace UnitTest
 {
-    public class ClientProfileTest
+    public class ClientProfileControllerTest
     {
         [Fact]
-        public void Test_SaveProfile_Post_Valid_Model()
+        public void Test_Profile_ReturnsViewWhenCookieIsNotNull()
         {
             var controller = new ClientProfileController();
-            var model = new ClientProfileModel
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers["Cookie"] = "username_cookie=admin";
+            controller.ControllerContext = new ControllerContext
             {
-                FullName = "John Doe",
-                Address1 = "123 Main St",
-                City = "Some City",
-                State = "TX",
-                Zipcode = "12345"
+                HttpContext = httpContext
             };
 
-            var result = controller.SaveProfile(model) as ViewResult;
+            var result = controller.Profile() as ViewResult;
+
             Assert.NotNull(result);
-            Assert.Equal("Profile", result.ViewName); 
-            Assert.Equal("Profile saved successfully!", model.SuccessMessage);
+            Assert.IsType<ClientProfileModel>(result.Model);
         }
 
         [Fact]
-        public void Test_SaveProfile_Post_Invalid_Model()
+        public void Test_Profile_RedirectsToLoginWhenCookieIsNull()
         {
             var controller = new ClientProfileController();
+            var httpContext = new DefaultHttpContext();
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+
+            var result = controller.Profile() as RedirectToActionResult;
+
+            Assert.NotNull(result);
+            Assert.Equal("login", result.ActionName);
+            Assert.Equal("Login", result.ControllerName);
+        }
+
+        [Fact]
+        public void Test_Profile_ReturnsViewWhenDatabaseQueryReturnsNoRows()
+        {
+            
+        }
+
+        [Fact]
+        public void Test_Profile_ReturnsViewWhenExceptionDuringDatabaseInteraction()
+        {
+
+        }
+
+        [Fact]
+        public void Test_Profile_ReturnsViewWhenDatabaseQueryReturnsRows()
+        {
+
+        }
+
+        [Fact]
+        public void Test_SaveProfile_UpdatesProfileWhenProfileExists()
+        {
+
+            var controller = new ClientProfileController();
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Headers["Cookie"] = "username_cookie=admin";
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+
+            var mockRepository = new Mock<IYourRepository>();
+
+            mockRepository.Setup(repo => repo.GetProfileByUsername(It.IsAny<string>()))
+                          .Returns(new ClientProfileModel { Address1 = "123 Main St", /* Set up other profile properties */ });
+
             var model = new ClientProfileModel
             {
-                FullName = null,
+                FullName = "Germa66", 
                 Address1 = "123 Main St",
-                City = "Some City",
+                City = "Houston",
                 State = "TX",
-                Zipcode = "12345"
+                Zipcode = "77056"
             };
-            controller.ModelState.AddModelError("FullName", "Full Name is required");
             var result = controller.SaveProfile(model) as ViewResult;
+
             Assert.NotNull(result);
             Assert.Equal("Profile", result.ViewName);
+            Assert.IsType<ClientProfileModel>(result.Model);
+            Assert.Equal("Profile saved successfully!", ((ClientProfileModel)result.Model).SuccessMessage);
+        }
+
+        [Fact]
+        public void Test_SaveProfile_InsertsNewProfileWhenProfileDoesNotExist()
+        {
+            
+        }
+
+        [Fact]
+        public void Test_SaveProfile_ReturnsViewWhenExceptionDuringDatabaseInteraction()
+        {
+           
         }
     }
 }
